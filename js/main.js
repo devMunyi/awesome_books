b58bd057dde22a8ef5d8e51c1c79d1686177c303
vitalis <vitalismutwiri2gmail.com>
2022-09-19 19:30:32 +0300

    Create Awesome books

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const books = JSON.parse(localStorage.getItem('books'))
+let books = JSON.parse(localStorage.getItem('books'))
   ? JSON.parse(localStorage.getItem('books'))
   : [];
 
@@ -13,27 +13,20 @@ function loadBooks() {
         <div class="remove_book-btn__container">
           <button onclick="removeBook(${book.id})" class="remove_book-btn">Remove</button>
         </div>
-    </div>`,
+    </div> <hr>`,
   );
-  console.log('books html => ', list);
-
   booksContainerElement.innerHTML = list.join('');
 }
 
-function preFillInputs() {}
-
-function populateLocalstorage(key, value, nextAction = '') {
+function populateLocalstorage(key, value) {
   localStorage.setItem(key, JSON.stringify(value));
-  console.log('nextAction ', nextAction);
-
-  nextAction === 'loadBooks' ? loadBooks() : '';
-  // nextAction === 'prefillinputs' ? preFillInputs() : '';
+  loadBooks();
 }
 
 // function to add a book
 function addBook(bookObject) {
   books.push(bookObject);
-  populateLocalstorage('books', books, 'loadBooks');
+  populateLocalstorage('books', books);
 }
 
 // get form element to keep track of user submit action
@@ -70,49 +63,9 @@ document.addEventListener('DOMContentLoaded', () => {
   loadBooks();
 });
 
-const removeBtn = document.get;
-
 function removeBook(id) {
   const booksAfterRemove = books.filter((book) => book.id !== id);
-  // console.log('books list after delete ', booksAfterRemove);
-
+  books = booksAfterRemove;
   populateLocalstorage('books', booksAfterRemove);
-  loadBooks();
 }
-
-// addBook(bookObj);
-
-// // function to remove a book
-// const removeBook = (id) => {
-//   const booksAfterRemove = books.filter((book) => book.id !== id);
-//   //update books with books list after one is removed
-//   books = booksAfterRemove;
-// };
-
-// removeBook(1);
-// console.log('BOOKS => ', books);
-
-// const booksInputFields = document.querySelectorAll('.book-input');
-
-// booksInputFields.forEach((inputField) => {
-//   inputField.addEventListener('change', () => {
-//     const formDataObj = {
-//       title: titleElement.value,
-//       author: authorElement.value,
-//     };
-//     populateLocalstorage('formData', formDataObj);
-//   });
-// });
-
-// function populateInputFields() {
-//   const formData = JSON.parse('formData');
-
-//   titleElement.value = formData.title;
-//   authorElement.value = formData.author;
-// }
-
-// if (!localStorage('formData')) {
-//   populateLocalstorage();
-// } else {
-//   populateInputFields();
-// }
+removeBook();
