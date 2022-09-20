0edff0e1f7b95fb9fd3a71ab57d23402311ec364
devMunyi <samunyi90@gmail.com>
2022-09-20 21:35:05 +0300

    refactor js to classes

--- js/main.js
+++ js/main.js
@@ -1,72 +1,75 @@
-let books = JSON.parse(localStorage.getItem('books'))
+let bookData = JSON.parse(localStorage.getItem('books'))
   ? JSON.parse(localStorage.getItem('books'))
   : [];
 
-function loadBooks() {
-  const booksContainerElement = document.getElementById('books');
+class Awesomebooks {
+  constructor(id, title, author) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+  }
 
-  const list = books.map(
-    (book) => `<div id="${book.id}" class="book">
-        <div class="book-title">${book.title}</div>
-        <div class="book-author">${book.author}</div>
-        <br />
-        <div class="remove_book-btn__container">
-          <button onclick="removeBook(${book.id})" class="remove_book-btn">Remove</button>
-        </div>
-    </div> 
-    <hr>`
-  );
-  booksContainerElement.innerHTML = list.join('');
+  static createBooks() {
+    const booksContainer = document.querySelector('.books-list');
+    if (bookData.length) {
+      const displayBooks = bookData
+        .map(
+          (book) => `
+        <div id="book_id" class="book">
+            <div class="title-and-author">
+            <div class="book-title">${book.title} by</div>
+            <div class="book-author">${book.author}</div>
+            </div>
+          <br />
+          <div class="remove_book-btn__container">
+            <button onclick="Awesomebooks.delBook(${book.id});" class="remove_book-btn">Remove</button>
+          </div>
+        </div><hr>
+  `
+        )
+
+        .join('');
+      booksContainer.innerHTML = displayBooks;
+    } else {
+      booksContainer.innerHTML =
+        ' <span class="text-center"><i>No books added yet</i></span>';
+    }
+  }
+
+  // Method to add books
+  static addBook(bookItem) {
+    bookData.push(bookItem);
+    // eslint-disable-next-line no-use-before-define
+    sendToLocal('books', bookData);
+  }
+
+  // Method to delete books
+  // eslint-disable-next-line no-unused-vars
+  static delBook(id) {
+    const filteredBooks = bookData.filter((item) => item.id !== id);
+    bookData = filteredBooks;
+    // eslint-disable-next-line no-use-before-define
+    sendToLocal('books', filteredBooks);
+  }
 }
-
-function populateLocalstorage(key, value) {
-  localStorage.setItem(key, JSON.stringify(value));
-  loadBooks();
-}
-
-// function to add a book
-function addBook(bookObject) {
-  books.push(bookObject);
-  populateLocalstorage('books', books);
-}
-
-// get form element to keep track of user submit action
-const form = document.getElementById('add-book-form');
-
-// get title element
-const titleElement = form.elements.title;
-
-// get author element
-const authorElement = form.elements.author;
-
-form.addEventListener('submit', (e) => {
-  // prevent form submission
-  e.preventDefault();
-  // get the count of books avilable in the books object
-  // so as to determine the id of the next book to be added
-  const booksCount = books.length;
-
-  // next book
-  let nextBookId = books[booksCount - 1]?.id ? books[booksCount - 1]?.id : 0;
-  nextBookId += 1;
-  // sample book object with title and author properties
-  const bookObj = {
-    id: nextBookId,
-    title: titleElement.value,
-    author: authorElement.value,
-  };
-
-  // call addBook function and pass this book
-  addBook(bookObj);
-});
-
 document.addEventListener('DOMContentLoaded', () => {
-  loadBooks();
+  Awesomebooks.createBooks();
 });
-
-/* eslint-disable no-unused-vars */
-function removeBook(id) {
-  const booksAfterRemove = books.filter((book) => book.id !== id);
-  books = booksAfterRemove;
-  populateLocalstorage('books', booksAfterRemove);
+function sendToLocal(a, b) {
+  localStorage.setItem(a, JSON.stringify(b));
+  Awesomebooks.createBooks();
 }
+document.getElementById('add-book-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const nextBookId = bookData.length + 1;
+
+  const bookObject = new Awesomebooks(
+    nextBookId,
+    document.getElementById('title').value,
+    document.getElementById('author').value
+  );
+  Awesomebooks.addBook(bookObject);
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+});
+Awesomebooks.delBook();
