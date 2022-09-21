24829d748d13c5417493dff75890a359071e8a92
devMunyi <samunyi90@gmail.com>
2022-09-21 12:15:10 +0300

    create getAddedBookId method

--- js/main.js
+++ js/main.js
@@ -1,21 +1,36 @@
 class Awesomebooks {
+  // object contructor metthod for setting object properties
   constructor(id, title, author) {
     this.id = id;
     this.title = title;
     this.author = author;
   }
 
+  // class method to get book list
   static getBookList() {
     return JSON.parse(localStorage.getItem('books'))
       ? JSON.parse(localStorage.getItem('books'))
       : [];
   }
 
-  static createBooks() {
+  // class method to get added book id
+  static getAddedBookId() {
+    const books = Awesomebooks.getBookList();
+    const booksCount = books.length,
+      lastBookIndex = booksCount - 1;
+    const lastAddedBookId = books[lastBookIndex]?.id
+      ? books[lastBookIndex].id
+      : 0;
+
+    return lastAddedBookId + 1;
+  }
+
+  // class method to render book list on user interface
+  static displayBookList() {
     const booksContainer = document.querySelector('.books-list');
     const bookData = Awesomebooks.getBookList();
     if (bookData.length) {
-      const displayBooks = bookData
+      const listOfBooks = bookData
         .map(
           (book) => `<div id="book_id" class="book">
             <div class="title-and-author">
@@ -24,54 +39,60 @@ class Awesomebooks {
             </div>
           <br />
           <div class="remove_book-btn__container">
-            <button onclick="Awesomebooks.delBook(${book.id});" class="remove_book-btn">Remove</button>
+            <button onclick="Awesomebooks.removeBook(${book.id});" class="remove_book-btn">Remove</button>
           </div>
-        </div><hr>`,
+        </div><hr>`
         )
         .join('');
-      booksContainer.innerHTML = displayBooks;
+      booksContainer.innerHTML = listOfBooks;
     } else {
-      booksContainer.innerHTML = ' <span class="text-center"><i>No books added yet</i></span>';
+      booksContainer.innerHTML =
+        ' <span class="text-center"><i>No books added yet</i></span>';
     }
   }
 
-  // Method to add books
+  // class method to persist book data in the localstorage
+  static sendToLocal(k, v) {
+    localStorage.setItem(k, JSON.stringify(v));
+    Awesomebooks.displayBookList();
+  }
+
+  // class method to a book booklist
   static addBook(bookItem) {
     const bookData = Awesomebooks.getBookList();
     bookData.push(bookItem);
     // eslint-disable-next-line no-use-before-define
-    sendToLocal('books', bookData);
+    Awesomebooks.sendToLocal('books', bookData);
   }
 
-  // Method to delete books
+  // class method to remove a book from booklist
   /* eslint-disable-next-line no-unused-vars */
-  static delBook(id) {
+  static removeBook(id) {
     const bookData = Awesomebooks.getBookList();
     const filteredBooks = bookData.filter((item) => item.id !== id);
     // bookData = filteredBooks;
+
     // eslint-disable-next-line no-use-before-define
-    sendToLocal('books', filteredBooks);
+    Awesomebooks.sendToLocal('books', filteredBooks);
   }
 }
+
+// listens to page content loaded event
 document.addEventListener('DOMContentLoaded', () => {
-  Awesomebooks.createBooks();
+  Awesomebooks.displayBookList();
 });
-function sendToLocal(a, b) {
-  localStorage.setItem(a, JSON.stringify(b));
-  Awesomebooks.createBooks();
-}
+
+// listens to form submission event
 document.getElementById('add-book-form').addEventListener('submit', (e) => {
   e.preventDefault();
-  const bookData = Awesomebooks.getBookList();
-  const nextBookId = bookData.length + 1;
 
+  const bookId = Awesomebooks.getAddedBookId();
   const bookObject = new Awesomebooks(
-    nextBookId,
+    bookId,
     document.getElementById('title').value,
-    document.getElementById('author').value,
+    document.getElementById('author').value
   );
   Awesomebooks.addBook(bookObject);
   document.getElementById('title').value = '';
   document.getElementById('author').value = '';
 });
-Awesomebooks.delBook();
